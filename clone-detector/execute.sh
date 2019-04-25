02e41c25c024f457dd3672e01fd4f449eaa8a582
Viktor Poletansky <poletansky@gmail.com>
2019-04-25 17:09:07 +0300

    Update execute.sh

--- clone-detector/execute.sh
+++ clone-detector/execute.sh
@@ -4,14 +4,8 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 scriptPATH=$(realpath "$0")
-<<<<<<< HEAD
-rootPATH=`dirname $scriptPATH`
-echo $rootPATH
-rm -rf $rootPATH/NODE*
-=======
 rootPATH=$(dirname $scriptPATH)
 printf "\e[32m[execute.sh] \e[0m$rootPATH\n"
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 num_nodes="${1:-2}"
 th="${2:-8}"
 queryfile="$rootPATH/input/dataset/blocks.file"
