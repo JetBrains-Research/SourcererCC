0d0b357bc39dbaa67149343f1da9f42cae8d86ce
Viktor Poletansky <poletansky@gmail.com>
2019-04-25 17:10:03 +0300

    Update preparequery.sh

--- clone-detector/preparequery.sh
+++ clone-detector/preparequery.sh
@@ -4,14 +4,8 @@ realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 num_nodes="${1:-0}"
-scriptPATH=$(realpath "$0")
-<<<<<<< HEAD
-rootPATH=`dirname $scriptPATH`
-echo "rootpath is : $rootPATH"
-=======
 rootPATH=$(dirname $scriptPATH)
 printf "\e[32m[preparequery.sh] \e[0mrootpath is : $rootPATH\n"
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 for i in $(seq 1 1 $num_nodes)
 do
   foldername="$rootPATH/NODE_$i/query/"
