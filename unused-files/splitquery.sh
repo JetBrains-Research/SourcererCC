89fe29fc9587f3db60cc7dfdaa451b7c12ceb9c5
Viktor Poletansky <poletansky@gmail.com>
2019-04-25 17:22:06 +0300

    Update splitquery.sh

--- unused-files/splitquery.sh
+++ unused-files/splitquery.sh
@@ -1,20 +1,12 @@
 #!/bin/bash
 
 # Configuration stuff
-<<<<<<< HEAD:clone-detector/splitquery.sh
 
-=======
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675:unused-files/splitquery.sh
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 scriptPATH=$(realpath "$0")
-<<<<<<< HEAD:clone-detector/splitquery.sh
-rootPATH=`dirname $scriptPATH`
-echo "inside splitquery "
-=======
 rootPATH=$(dirname $scriptPATH)
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675:unused-files/splitquery.sh
 queryfile="$rootPATH/input/dataset/blocks.file"
 num_files="${1:-2}"
 
