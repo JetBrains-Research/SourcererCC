1205c6e468777bc8259da1488f4c40c04e9104c4
Viktor Poletansky <poletansky@gmail.com>
2019-04-25 17:07:25 +0300

    Update backup-gtpm.sh

--- clone-detector/backup-gtpm.sh
+++ clone-detector/backup-gtpm.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-<<<<<<< HEAD
-#
-#
-=======
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 scriptPATH=$(realpath "$0")
-<<<<<<< HEAD
-rootPATH=`dirname $scriptPATH`
-echo "backing up gtpm indexes..."
-=======
 rootPATH=$(dirname $scriptPATH)
 printf "\e[32m[backup-gtpm.sh] \e[0mbacking up gtpm indexes...\n"
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 rm -rf $rootPATH/backup_gtpm
 mkdir $rootPATH/backup_gtpm
 cp -r $rootPATH/gtpmindex $rootPATH/backup_gtpm
