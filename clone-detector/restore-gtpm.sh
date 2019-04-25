756be2e34e13955ab3b59f9eb83348f91f5e6cbe
Viktor Poletansky <poletansky@gmail.com>
2019-04-25 17:11:55 +0300

    Update restore-gtpm.sh

--- clone-detector/restore-gtpm.sh
+++ clone-detector/restore-gtpm.sh
@@ -1,21 +1,11 @@
 #!/bin/bash
-<<<<<<< HEAD
-#
-#
-=======
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 
 realpath() {
     [[ $1 = /* ]] && echo "$1" || echo "$PWD/${1#./}"
 }
 scriptPATH=$(realpath "$0")
-<<<<<<< HEAD
-rootPATH=`dirname $scriptPATH`
-echo "restoring gtpm indexes..."
-=======
 rootPATH=$(dirname $scriptPATH)
 printf "\e[32m[restore-gtpm.sh] \e[0mrestoring gtpm indexes...\n"
->>>>>>> 2e73ed3bfbff4d5bbd45af8ad38e71a185fb8675
 if [ -d "$rootPATH/gtpmindex" ]; then
    rm -rf $rootPATH/gtpmindex
 fi
