fcf3e7540eff5e209fe2c3cf63755995ab59eb6e
Viktor Poletansky <poletansky@gmail.com>
2019-07-02 17:47:56 +0300

    Fix build
    Fix build testing and add tests cases for file tokenizer

--- tokenizers/tokenizing/file_tokenizer_tests.py
+++ tokenizers/tokenizing/file_tokenizer_tests.py
@@ -1,16 +1,25 @@
+import os
 import re
 import unittest
 
-from .file_tokenizer import *
+from .file_tokenizer import read_config, tokenize_files
+from .utils import md5_hash
 
 
-REGEX = re.compile('.+@@::@@\d+')
-tokenizing.read_config()
+REGEX = re.compile(r".+@@::@@\d+")
+read_config("file_config.ini")
 
 
 class TestParser(unittest.TestCase):
-    # Input is something like: @#@print@@::@@1,include@@::@@1,sys@@::@@1
+    def run_on_test_file(self, file_name):
+        """ Test tokenizer on file which can break tokenizer due to recursion limit """
+        source_content = ""
+        with open(os.path.join(os.path.dirname(os.path.abspath(__file__)), file_name), "r", encoding="utf-8") as fd:
+            source_content = fd.read()
+        return tokenize_files(source_content)
+
     def assert_common_properties(self, list_tokens_string):
+        """ Input is something like: @#@print@@::@@1,include@@::@@1,sys@@::@@1 """
         self.assertTrue(list_tokens_string.startswith('@#@'))
 
         if len(list_tokens_string) > 3:
@@ -18,28 +27,38 @@ class TestParser(unittest.TestCase):
             for pair in split:
                 self.assertTrue(REGEX.match(pair))
 
+    def assert_line_counts(self, res, lines, LOC, SLOC):
+        (final_stats, _, _) = res
+        (_, actual_lines, actual_LOC, actual_SLOC) = final_stats
+
+        self.assertEqual(actual_lines, lines)
+        self.assertEqual(actual_LOC, LOC)
+        self.assertEqual(actual_SLOC, SLOC)
+
+    def assert_tokenization_results(self, res, lines, LOC, SLOC, total_tokens, unique_tokens):
+        (_, final_tokens, _) = res
+        (actual_tokens_count_total, actual_tokens_count_unique, _, tokens) = final_tokens
+
+        self.assert_line_counts(res, lines=lines, LOC=LOC, SLOC=SLOC)
+
+        self.assertEqual(actual_tokens_count_total, total_tokens)
+        self.assertEqual(actual_tokens_count_unique, unique_tokens)
+        self.assert_common_properties(tokens)
+
     def test_line_counts_1(self):
         input_str = """ line 1
                         line 2
                         line 3 """
-        (final_stats, _, _) = tokenize_files(input_str)
-        (_, lines, LOC, SLOC) = final_stats
-
-        self.assertEqual(lines, 3)
-        self.assertEqual(LOC, 3)
-        self.assertEqual(SLOC, 3)
+        res = tokenize_files(input_str)
+        self.assert_line_counts(res, lines=3, LOC=3, SLOC=3)
 
     def test_line_counts_2(self):
         input_str = """ line 1
                         line 2
                         line 3
                     """
-        (final_stats, _, _) = tokenize_files(input_str)
-        (_, lines, LOC, SLOC) = final_stats
-
-        self.assertEqual(lines, 4)
-        self.assertEqual(LOC, 3)
-        self.assertEqual(SLOC, 3)
+        res = tokenize_files(input_str)
+        self.assert_line_counts(res, lines=4, LOC=3, SLOC=3)
 
     def test_line_counts_3(self):
         input_str = """ line 1
@@ -47,40 +66,19 @@ class TestParser(unittest.TestCase):
                     // line 2
                     line 3 
                 """
-        (final_stats, _, _) = tokenize_files(input_str)
-        (_, lines, LOC, SLOC) = final_stats
-
-        self.assertEqual(lines, 5)
-        self.assertEqual(LOC, 3)
-        self.assertEqual(SLOC, 2)
+        res = tokenize_files(input_str)
+        self.assert_line_counts(res, lines=5, LOC=3, SLOC=2)
 
     def test_comments(self):
-        input_str = "// Hello\n // World"
-        (final_stats, final_tokens, _) = tokenize_files(input_str)
-        (_, lines, LOC, SLOC) = final_stats
-        (tokens_count_total, tokens_count_unique, _, tokens) = final_tokens
-
-        self.assertEqual(lines, 2)
-        self.assertEqual(LOC, 2)
-        self.assertEqual(SLOC, 0)
-
-        self.assertEqual(tokens_count_total, 0)
-        self.assertEqual(tokens_count_unique, 0)
-        self.assert_common_properties(tokens)
+        input_str = """// Hello
+         // World"""
+        res = tokenize_files(input_str)
+        self.assert_tokenization_results(res, lines=2, LOC=2, SLOC=0, total_tokens=0, unique_tokens=0)
 
     def test_multiline_comment(self):
         input_str = '/* this is a \n comment */ /* Last one */ '
-        (final_stats, final_tokens, _) = tokenize_files(input_str)
-        (_, lines, LOC, SLOC) = final_stats
-        (tokens_count_total, tokens_count_unique, _, tokens) = final_tokens
-
-        self.assertEqual(lines, 2)
-        self.assertEqual(LOC, 2)
-        self.assertEqual(SLOC, 0)
-
-        self.assertEqual(tokens_count_total, 0)
-        self.assertEqual(tokens_count_unique, 0)
-        self.assert_common_properties(tokens)
+        res = tokenize_files(input_str)
+        self.assert_tokenization_results(res, lines=2, LOC=2, SLOC=0, total_tokens=0, unique_tokens=0)
 
     def test_simple_file(self):
         string = u"""#include GLFW_INCLUDE_GLU
@@ -95,27 +93,47 @@ class TestParser(unittest.TestCase):
                        }
                        printf("%s", "asciiじゃない文字");
                      }"""
-        (final_stats, final_tokens, _) = tokenize_files(string)
-        (_, lines, LOC, SLOC) = final_stats
-        (tokens_count_total, tokens_count_unique, token_hash, tokens) = final_tokens
-
-        self.assertEqual(lines, 12)
-        self.assertEqual(LOC, 11)
-        self.assertEqual(SLOC, 9)
-
-        self.assertEqual(tokens_count_total, 27)
-        self.assertEqual(tokens_count_unique, 21)
-        self.assert_common_properties(tokens)
+        res = tokenize_files(string)
+        (_, final_tokens, _) = res
+        (_, _, token_hash, tokens) = final_tokens
+        self.assert_tokenization_results(res, lines=12, LOC=11, SLOC=9, total_tokens=27, unique_tokens=21)
 
         hard_tokens = {'int@@::@@4', 'void@@::@@1', 'cstdio@@::@@1', 'action@@::@@1', 'static@@::@@1', 'key@@::@@1',
-                       'glfw_key_callback@@::@@1', 'mod@@::@@1', 'if@@::@@1', 'glfw3@@::@@1', 'scancode@@::@@1',
-                       'h@@::@@1', 'GLFW_INCLUDE_GLU@@::@@1', 'input_event_queue@@::@@2', 'GLFW@@::@@1', 'push@@::@@1',
-                       'inputaction@@::@@1', 'include@@::@@3', 'asciiじゃない文字@@::@@1'}
-        this_tokens = set(tokens[3:].split(','))
-        self.assertTrue(len(hard_tokens - this_tokens), 0)
+                       'glfw_key_callback@@::@@2', 'mod@@::@@1', 'if@@::@@1', 'glfw3@@::@@1', 'scancode@@::@@1',
+                       'h@@::@@1', 'GLFW_INCLUDE_GLU@@::@@1', 'input_event_queue@@::@@1', 'GLFW@@::@@1', 'push@@::@@1',
+                       'inputaction@@::@@1', 'include@@::@@3', 'asciiじゃない文字@@::@@1', 'printf@@::@@1', 's@@::@@1'}
+        tokens_str = tokens[3:]
+        self.assertSetEqual(set(tokens_str.split(',')), set(hard_tokens))
+        self.assertEqual(md5_hash(tokens_str), token_hash)
 
-        self.assertEqual(md5_hash(tokens[3:]), token_hash)
+    def test_recursion_limit_light(self):
+        """ Test tokenizer on file which can break tokenizer because of lots of
+        string concatenations and therefore recursion limit """
+        self.run_on_test_file("tests/RecursionLimit.java")
 
+    def test_recursion_limit_heavy(self):
+        """ Test tokenizer on file with more string concatenations """
+        self.run_on_test_file("tests/RecursionLimit2.java")
+
+    def test_wrong_syntax_error(self):
+        """ Test tokenizer not failing on constructions like String[]::new """
+        self.run_on_test_file("tests/SyntaxError.java")
+
+    def test_unicode_comments(self):
+        """ Test tokenizer working with non-ascii chars in comments """
+        res = self.run_on_test_file("tests/UnicodeComments.java")
+
+        self.assert_tokenization_results(res, lines=10, LOC=10, SLOC=5, total_tokens=14, unique_tokens=13)
+
+    def test_unicode_methodname(self):
+        """ Test tokenizer working with non-ascii chars in methodnames """
+        res = self.run_on_test_file("tests/UnicodeMethodName.java")
+        self.assert_tokenization_results(res, lines=9, LOC=8, SLOC=8, total_tokens=20, unique_tokens=16)
+
+    def test_string_literal(self):
+        """ Test tokenizer working with non-ascii chars in string literals """
+        res = self.run_on_test_file("tests/UnicodeStringLiteral.java")
+        self.assert_tokenization_results(res, lines=6, LOC=6, SLOC=6, total_tokens=21, unique_tokens=17)
 
 if __name__ == '__main__':
     unittest.main()
---
+++ tokenizers/tokenizing/tests/UnicodeStringLiteral.java
@@ -0,0 +1,6 @@
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Привет, Мир!");
+        System.out.println("I ❤ Chinese (侘寂)");
+    }
+}
